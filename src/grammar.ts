8f6ea31c782eccd81aed26151e107009355863e7
amlo <xplzjwz@gmail.com>
2021-04-14 21:31:37 +0800

    init

---
+++ src/grammar.ts
@@ -0,0 +1,8 @@
+import { Rule } from "./rule"
+
+class Grammar {
+}
+
+export const grammar = (rules: []Rule): Grammar => {
+  return new Grammar
+}
---
+++ src/rule.ts
@@ -0,0 +1,5 @@
+
+export class Rule {
+}
+
+export const rule = 1
